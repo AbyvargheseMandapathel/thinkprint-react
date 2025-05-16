e3bd730f68d37de33d09922b8f04b70ee9133b75
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-05-17 00:23:22 +0530

    image upload issue

--- api/categories-add.js
+++ api/categories-add.js
@@ -8,7 +8,7 @@ const dbConfig = {
   database:  'u911622560_thinkprint',
   waitForConnections: true,
   connectionLimit: 10,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false }
 };
 
 export default async function handler(req, res) {
