d20085da9fd5b1d67a3df039b3e0558104d30414
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-05-17 22:49:42 +0530

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -110,8 +110,8 @@ const App = () => {
           />
 
           {/* Other Routes */}
-          <Route path="/products" element={<ProductListPage products={products.filter(p => p && p.is_urban === false) || []} />} />
-          <Route path="/urbangear" element={<ProductListPage products={products.filter(p => p && p.is_urban === true) || []} />} />
+          <Route path="/products" element={<ProductListPage products={products.filter(p => p && p.is_urbangear === false) || []} />} />
+          <Route path="/urbangear" element={<ProductListPage products={products.filter(p => p && p.is_urbangear === true) || []} />} />
           <Route path="/product/:id" element={<ProductDetailPage />} />
           <Route path="/search" element={<SearchResult />} />
           <Route path="/category/:categoryName" element={<ProductListPage products={products} />} />
