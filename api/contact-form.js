b9f4587b8daab9cea04429bd3f30ad6ae30fbbc0
Aby varghese <56151184+AbyvargheseMandapathel@users.noreply.github.com>
2025-06-08 18:08:17 +0530

    Update contact-form.js
    Added localhost:3000

--- api/contact-form.js
+++ api/contact-form.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   // Define allowed origins
-  const allowedOrigins = ['http://localhost:5173', 'https://thinkprint.shop'];
+  const allowedOrigins = ['http://localhost:5173', 'https://thinkprint.shop','http://localhost:3000'];
   const origin = req.headers.origin;
 
   // Set CORS headers if the origin is allowed
